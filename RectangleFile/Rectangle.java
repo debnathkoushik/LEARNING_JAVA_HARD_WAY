51d51e05c1a2a338a988d42682d3f97ba2869eb7
Koushik Debnath <debnathkoushik605@gmail.com>
2021-12-03 20:31:52 +0530

    more practices on polymorphism

---
+++ RectangleFile/Rectangle.java
@@ -0,0 +1,22 @@
+package RectangleFile;
+
+public class Rectangle
+{
+    private int length, width;
+    
+    public Rectangle()
+    {
+        length = width = 0;
+    }
+    
+    public Rectangle( int l, int w )
+    {
+        length = l;
+        width = w;
+    }
+    
+    public int getArea()
+    {
+        return length*width;
+    }
+}
