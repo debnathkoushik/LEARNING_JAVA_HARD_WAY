6bedd44ae9d98a02807d24028b92cc49a755e7b2
debnathkoushik <debnathkoushik605@gmail.comm>
2023-01-11 13:09:01 +0530

    a code to call a function from a different class

---
+++ practiceFile1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class demo
+{
+    public void function()
+    {
+        System.out.println("Object testing successful!");
+    }
+}
+
+public class practiceFile1
+{
+    public static void main(String args[])
+    {
+        demo object = new demo();
+        object.function();
+        
+        System.out.println("Success!");
+    }
+}
